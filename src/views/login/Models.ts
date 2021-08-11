84685cb1fa13c01d91a071f493b2a56f15e88f20
mekala <mekala@yectra.com>
2021-08-11 15:13:34 +0530

    requestmodel created

---
+++ src/views/login/Models.ts
@@ -0,0 +1,12 @@
+export class RegistrationRequestModel {
+    FirstName: string;
+    LastName: string;
+    EmailAddress: string;
+    Password: string;
+    ConfirmPassword: string;
+    GSTNumber: number;
+    CountryId: number;
+    StateId: number;
+    CityId: number;
+    ZipCode: number;
+}

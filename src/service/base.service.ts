94830e60dd0731d51e8b3135565aed6890b47059
Kumar R <kumar@yectra.com>
2021-12-10 02:18:55 +0530

    Reorganize service files

--- src/service/base.service.ts
+++ src/service/base.service.ts
@@ -5,26 +5,12 @@ import { plainToClass } from 'class-transformer';
 
 import { Settings } from '@/config';
 
-import { DataResponse, IBaseModel, IBaseRequest, PageInfo, ListItem, DataRequest } from '@/model';
-
-export interface IBaseService<R extends IBaseRequest, T extends IBaseModel> {
-    getItem(id: string): Promise<T>;
-    getItems(request: R): Promise<DataResponse<T>>;
-    getAll(): Promise<DataResponse<T>>;
-
-    post(data: T, endPoint?: string): Promise<T>;
-    put(data: T, endPoint?: string): Promise<T>;
-    delete(id: string): Promise<boolean>;
-    export(request: R, fileName: string, endPoint?: string): void;
-}
 export interface EntityType<T> {
     new(): T;
 }
 
-export abstract class ServiceHelper { 
+export abstract class ServiceHelper {
     protected baseUrl: string = Settings.ApiUrl;
-
-    protected apiUrl: string = `${this.baseUrl}/${Settings.ApiPath}`;
 
     protected deserialize<T>(classType: EntityType<T>, source: any): T {
         const raw = plainToClass<T, any>(classType, source);
@@ -57,7 +43,7 @@ export abstract class ServiceHelper {
             }
         }
 
-        const path = `${this.apiUrl}/${route}`;
+        const path = `${this.baseUrl}/${route}`;
         const config: AxiosRequestConfig = {
             params: params,
             'paramsSerializer': params => {
@@ -71,189 +57,21 @@ export abstract class ServiceHelper {
     }
 
 
-    protected httpPost(route: string, data: any,config?:AxiosRequestConfig): Promise<AxiosResponse<any>> {
-        const path = `${this.apiUrl}/${route}`;
+    protected httpPost(route: string, data: any, config?: AxiosRequestConfig): Promise<AxiosResponse<any>> {
+        const path = `${this.baseUrl}/${route}`;
 
-        return axios.post(path, data,config);
+        return axios.post(path, data, config);
     }
 
     protected httpPut(route: string, data: any): Promise<AxiosResponse<any>> {
-        const path = `${this.apiUrl}/${route}`;
+        const path = `${this.baseUrl}/${route}`;
 
         return axios.put(path, data);
     }
 
     protected httpDelete(route: string): Promise<AxiosResponse<any>> {
-        const path = `${this.apiUrl}/${route}`;
+        const path = `${this.baseUrl}/${route}`;
 
         return axios.delete(path);
-    }
-}
-
-export class BaseService<R extends IBaseRequest, T extends IBaseModel> extends ServiceHelper {
-
-    protected eType?: EntityType<T>;
-    
-    constructor(protected path: string, apiUrl?: string) {
-
-        super();
-        if(apiUrl) {
-            this.apiUrl = apiUrl; 
-            console.log(`${apiUrl} ${this.apiUrl}`);
-        }
-        
-    }
-
-    protected format(item: T) { }
-
-    protected construct(data: any) {
-        if (this.eType) {
-            const item: T = this.deserialize<T>(this.eType, data);
-
-            this.format(item);
-            return item;
-        }
-        else
-            return data;
-    }
-
-    getItem(id: string): Promise<T> {
-        let path = this.path;
-        path = `${path}/${id}`;
-
-        return this.httpGet(path, null).then(
-            (response: AxiosResponse) => {
-                return response.data;
-            },
-            (reason: AxiosError) => {
-                return null;
-            }
-        );
-    }
-
-    getAll(): Promise<DataResponse<T>> {
-        const request: any = new DataRequest();
-        request._skipPaging = true;
-
-        return this.getItems(request);
-    }
-
-    getItems(request: R, additionalPath?:string): Promise<DataResponse<T>> {
-        if (request._skipPaging) {
-            //request.page = undefined;
-            //request.pageSize = undefined;
-        }
-
-        let path = '';
-        if(additionalPath) path = `${this.path}/${additionalPath}`
-        else path = this.path;
-
-        request._loading = true;
-        return this.httpGet(`${path}`, request).then(
-            (response: AxiosResponse) => {
-                request._loading = false;
-
-                return response.data;
-            },
-            (reason: AxiosError) => {
-                request._loading = false;
-
-                throw reason;
-            }
-        );
-    }
-
-    post(data: T, endPoint?: string): Promise<T> {
-        console.log(`${endPoint} ${this.apiUrl}`);
-        let path = this.path;
-        if (endPoint) path = `${path}/${endPoint}`;
-
-        return this.httpPost(path, data).then(response => {
-            return response.data;
-        });
-    }
-
-    protected searchPost(request: R, endPoint?: string): Promise<DataResponse<T>> {
-        /*if (request._skipPaging) {
-            request.page = undefined;
-            request.pageSize = undefined;
-        }*/
-
-        request._loading = true;
-
-        let path = this.path;
-        if (endPoint) path = `${path}/${endPoint}`;
-
-        return this.httpPost(path, request).then(
-            (response: AxiosResponse) => {
-                request._loading = false;
-
-                let result = new DataResponse<T>();
-
-                if (response.data) {
-                    const items: T[] = [];
-
-                    if (response.data.data) {
-                        response.data.data.forEach((item: any) => {
-                            items.push(this.construct(item));
-                        });
-                    }
-
-                    result = response.data;
-                    result.data = items;
-                }
-
-                return result;
-            },
-            (reason: AxiosError) => {
-                request._loading = false;
-
-                throw reason;
-            }
-        );
-    }
-
-    put(data: T, endPoint?: string): Promise<T> {
-        let path = this.path;
-        if (endPoint) path = `${path}/${endPoint}`;
-
-        return this.httpPut(path, data).then(response => {
-            return response.data;
-        });
-    }
-
-    delete(id: string): Promise<boolean> {
-        const path = `${this.path}/${id}`;
-
-        return this.httpDelete(path).then((response: AxiosResponse) => {
-            return true;
-        });
-    }
-
-    upload(formData: FormData, route: string) {
-        const path = `${this.path}/${route}`;
-
-        const config: AxiosRequestConfig = { headers: { 'Content-Type': 'multipart/form-data' } };
-
-        return axios.post(`${this.apiUrl}/${path}`, formData, config).then(response => {
-            return response.data;
-        });
-    }
-
-    export(request: R, fileName: string, endPoint?: string): void {
-        let path = `${this.path}/export`;
-        if (endPoint) path = endPoint;
-
-        this.httpGet(path, request, "blob").then(response => {
-            const fileUrl = window.URL.createObjectURL(new Blob([response.data]));
-            const fileLink = document.createElement("a");
-            fileLink.href = fileUrl;
-            fileLink.setAttribute("download", fileName);
-            fileLink.style.display = "none";
-            document.body.appendChild(fileLink);
-
-            fileLink.click();
-            document.body.removeChild(fileLink);
-        });
     }
 }

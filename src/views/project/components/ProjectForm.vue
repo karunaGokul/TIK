bca88ad514ec52ae3e16d47c18cf6822a266c95c
Karuna M <karuna@yectra.com>
2022-01-27 20:24:46 +0530

    fix

--- src/views/project/components/ProjectForm.vue
+++ src/views/project/components/ProjectForm.vue
@@ -354,11 +354,11 @@ export default class ProjectForm extends Vue {
           id: "deliveryDate",
           type: "date",
         },
-        // {
-        //   label: "Your required Credit Period in days",
-        //   id: "creditPeriod",
-        //   type: "number",
-        // },
+        {
+          label: "Your required Credit Period in days",
+          id: "creditPeriod",
+          type: "number",
+        },
       ],
     },
     {
@@ -384,11 +384,11 @@ export default class ProjectForm extends Vue {
           id: "price",
           type: "price",
         },
-        // {
-        //   label: "Your required Credit Period in days",
-        //   id: "creditPeriod",
-        //   type: "number",
-        // },
+        {
+          label: "Your required Credit Period in days",
+          id: "creditPeriod",
+          type: "number",
+        },
         // {
         //   label: "Your required Delivery Period in days",
         //   id: "deliveryPeriod",

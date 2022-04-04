da6116e80aaa91cce9afa6054cbf2f9fac3c2aca
Pugal bharathi <pugalbharthi61199@gmail.com>
2022-04-04 13:36:32 +0530

    adding columns

--- src/views/dashboard/ProjectList.vue
+++ src/views/dashboard/ProjectList.vue
@@ -6,7 +6,7 @@
           <v-icon large dark class="teal"> mdi-home</v-icon>
         </router-link>
         <v-icon large> mdi-chevron-right</v-icon>
-        {{ stagesRequest.replace(/([a-zA-Z])([A-Z])([a-z])/g, "$1 $2$3") }}
+        {{ stagesRequest.replace(/([a-zA-Z])([A-Z])([a-z])/g, '$1 $2$3') }}
       </v-col>
     </v-row>
 
@@ -19,14 +19,14 @@
               (category !== 'Company' &&
                 searchRequest.stages !== 'Initiated' &&
                 searchRequest.stages !== 'Cancelled')) &&
-                this.stagesRequest !== 'ProjectReviews'
+              this.stagesRequest !== 'ProjectReviews'
           "
           >my project</v-tab
         >
         <v-tab
           @click="searchProject('false')"
           v-if="
-            (category === 'Company'  ||
+            (category === 'Company' ||
               (category !== 'Company' &&
                 searchRequest.stages !== 'Initiated' &&
                 searchRequest.stages !== 'Cancelled')) &&
@@ -81,7 +81,7 @@
           <router-link
             :to="{
               name: 'ProjectDetail',
-              params: { id: item.Id, status: stagesRequest },
+              params: { id: item.Id, status: stagesRequest }
             }"
             tag="button"
           >
@@ -99,93 +99,93 @@
 </template>
 
 <script lang="ts">
-import { Component, Inject, Prop, Vue } from "vue-property-decorator";
-import { IDashboardService } from "@/service";
+import { Component, Inject, Prop, Vue } from 'vue-property-decorator';
+import { IDashboardService } from '@/service';
 import {
   DashboardModel,
   DashboardRequestModel,
-  ProjectSearchModel,
-} from "@/model";
+  ProjectSearchModel
+} from '@/model';
 @Component({})
 export default class ProjectList extends Vue {
-  @Inject("DashboardService") service: IDashboardService;
+  @Inject('DashboardService') service: IDashboardService;
 
   public response: Array<DashboardModel> = [];
-  public search: string = "";
+  public search: string = '';
   public stagesRequest: string;
   public searchToggle: boolean = false;
   public searchRequest = new ProjectSearchModel();
-  public stages: string = "";
+  public stages: string = '';
   public loading: boolean = false;
-  public values: string = "";
+  public values: string = '';
   public autocomplete: boolean = false;
   public request = new DashboardRequestModel();
 
   created() {
     this.stagesRequest = this.$route.params.status;
 
-    if (this.stagesRequest === "ProjectReviews") {
+    if (this.stagesRequest === 'ProjectReviews') {
       this.searchProject(false);
     }
-    if (this.category === "Company") {
-      if (this.role === "MasterAdmin" || this.role === "Approval Admin") {
+    if (this.category === 'Company') {
+      if (this.role === 'MasterAdmin' || this.role === 'Approval Admin') {
         this.searchProject(false);
       } else if (
-        (this.role === "Quote InCharge" || this.role === "Merchandiser") &&
-        (this.stagesRequest === "NewProjects" ||
-          this.stagesRequest === "BidReceived" ||
-          this.stagesRequest === "AwaitingAuthentication" ||
-          this.stagesRequest === "ApprovalPending" ||
-          this.stagesRequest === "NoResponse" ||
-          this.stagesRequest === "NoShow" ||
-          this.stagesRequest === "CancelledProjects" ||
-          this.stagesRequest === "ConfirmedProjects" ||
-          this.stagesRequest === "CompletedProjects")
+        (this.role === 'Quote InCharge' || this.role === 'Merchandiser') &&
+        (this.stagesRequest === 'NewProjects' ||
+          this.stagesRequest === 'BidReceived' ||
+          this.stagesRequest === 'AwaitingAuthentication' ||
+          this.stagesRequest === 'ApprovalPending' ||
+          this.stagesRequest === 'NoResponse' ||
+          this.stagesRequest === 'NoShow' ||
+          this.stagesRequest === 'CancelledProjects' ||
+          this.stagesRequest === 'ConfirmedProjects' ||
+          this.stagesRequest === 'CompletedProjects')
       ) {
         this.searchProject(true);
       }
     } else {
-      if (this.role === "MasterAdmin" || this.role === "Approval Admin") {
+      if (this.role === 'MasterAdmin' || this.role === 'Approval Admin') {
         this.searchProject(false);
       } else if (
-        this.role === "Quote InCharge" &&
-        (this.stagesRequest === "Approved" ||
-          this.stagesRequest === "AwaitingApproval" ||
-          this.stagesRequest === "NoResponse" ||
-          this.stagesRequest === "NoShow" ||
-          this.stagesRequest === "ConfirmedProjects" ||
-          this.stagesRequest === "Completed")
+        this.role === 'Quote InCharge' &&
+        (this.stagesRequest === 'Approved' ||
+          this.stagesRequest === 'AwaitingApproval' ||
+          this.stagesRequest === 'NoResponse' ||
+          this.stagesRequest === 'NoShow' ||
+          this.stagesRequest === 'ConfirmedProjects' ||
+          this.stagesRequest === 'Completed')
       ) {
         this.searchProject(true);
       } else if (
-        this.category !== "Company" &&
-        (this.stagesRequest === "Initiated" ||
-          this.stagesRequest === "Cancelled")
+        this.category !== 'Company' &&
+        (this.stagesRequest === 'Initiated' ||
+          this.stagesRequest === 'Cancelled')
       ) {
         this.searchProject(false);
       }
     }
-    if (this.category != "Company") {
+    if (this.category != 'Company') {
       this.headers.find((o: any) => {
-        if (o.text === "Merchandiser") {
-          o.text = "Approval Admin";
-          o.value = "ApprovalAdmin";
+        if (o.text === 'Merchandiser') {
+          o.text = 'Approval Admin';
+          o.value = 'ApprovalAdmin';
         }
       });
       this.items.splice(1, 5);
       this.items.push(
-        "Initiated",
-        "AwaitingApproval",
-        "Approved",
-        "Confirmed",
-        "Completed",
-        "Rejected"
+        'Initiated',
+        'AwaitingApproval',
+        'Approved',
+        'Confirmed',
+        'Completed',
+        'Rejected'
       );
     }
   }
 
   get showTabs(): boolean {
-    return this.role === "Merchandiser" || this.role === "Quote InCharge";
+    return this.role === 'Merchandiser' || this.role === 'Quote InCharge';
   }
 
   get category(): string {
@@ -197,64 +197,73 @@ export default class ProjectList extends Vue {
   }
 
   public searchProject(myproject: boolean) {
-    if (this.stagesRequest == "ProjectReviews") {
-      this.service.PendingReview().then((response) => {
+    if (this.stagesRequest == 'ProjectReviews') {
+      this.service.PendingReview().then(response => {
         this.response = response;
       });
     } else {
       this.searchRequest.myproject = myproject;
       this.searchRequest.stages = this.stagesRequest;
       this.loading = true;
-      this.service
-        .GetProjectListByFilter(this.searchRequest)
-        .then((response) => {
-          this.loading = false;
-          this.response = response;
+      this.service.GetProjectListByFilter(this.searchRequest).then(response => {
+        this.loading = false;
+        this.response = response;
 
-          this.searchToggle = true;
-        });
+        this.searchToggle = true;
+      });
     }
   }
 
   items: any = [
-    "All",
-    "New Enquiry",
-    "Submitted Enquirys",
-    "Confirmed Projects",
-    "Completed Projects",
+    'All',
+    'New Enquiry',
+    'Submitted Enquirys',
+    'Confirmed Projects',
+    'Completed Projects'
   ];
   headers: any = [
     {
-      text: "Enquiry Name",
-      align: "start",
-      value: "EnquiryName",
-      class: "teal lighten-3 subtitle-2",
+      text: 'Enquiry Name',
+      align: 'start',
+      value: 'EnquiryName',
+      class: 'teal lighten-3 subtitle-2'
     },
     {
-      text: "Merchandiser",
-      value: "Merchandiser",
-      class: "teal lighten-3 subtitle-2",
+      text: 'Merchandiser',
+      value: 'Merchandiser',
+      class: 'teal lighten-3 subtitle-2'
     },
-    { text: "Category", value: "Category", class: "teal lighten-3 subtitle-2" },
+    { text: 'Category', value: 'Category', class: 'teal lighten-3 subtitle-2' },
     {
-      text: "Subcategory",
-      value: "Subcategory",
-      class: "teal lighten-3 subtitle-2",
+      text: 'Subcategory',
+      value: 'Subcategory',
+      class: 'teal lighten-3 subtitle-2'
     },
     {
-      text: "In Stages",
-      value: "InStages",
-      class: "teal lighten-3 subtitle-2",
+      text: 'In Stages',
+      value: 'InStages',
+      class: 'teal lighten-3 subtitle-2'
     },
-    { text: "Status", value: "Status", class: "teal lighten-3 subtitle-2" },
+    {
+      text: 'Bids Received',
+      value: '',
+      class: 'teal lighten-3 subtitle-2'
+    },
+    {
+      text: 'Bid Time',
+      value: '',
+      class: 'teal lighten-3 subtitle-2'
+    },
+
+    { text: 'Status', value: 'Status', class: 'teal lighten-3 subtitle-2' },
 
     {
-      text: "Action",
-      value: "Action",
+      text: 'Action',
+      value: 'Action',
       sortable: false,
-      align: "center",
-      class: "teal lighten-3 subtitle-2",
-    },
+      align: 'center',
+      class: 'teal lighten-3 subtitle-2'
+    }
   ];
 }
 </script>
